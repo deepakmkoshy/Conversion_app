16cf1345cd4499c5242e154412648cc79214aa52
Deepak <deepakmkoshy@gmail.com>
2020-07-10 22:24:41 +0530

    Completed category_route file and created a listview in the main homescreen

--- lib/main.dart
+++ lib/main.dart
@@ -3,17 +3,19 @@
 // found in the LICENSE file.
 
 // You can read about packages here: https://flutter.io/using-packages/
+import 'package:conversion_app/category_route.dart';
 import 'package:flutter/material.dart';
 
 // You can use a relative import, i.e. `import 'category.dart';` or
 // a package import, as shown below.
 // More details at http://dart-lang.github.io/linter/lints/avoid_relative_lib_imports.html
 import 'package:conversion_app/category.dart';
+import 'category_route.dart';
 
 // TODO: Pass this information into your custom [Category] widget
-const _categoryName = 'Cake';
+/*const _categoryName = 'Cake';
 const _categoryIcon = Icons.cake;
-const _categoryColor = Colors.green;
+const _categoryColor = Colors.green;*/
 
 /// The function that is called when main.dart is run.
 void main() {
@@ -21,25 +23,14 @@ void main() {
 }
 
 /// This widget is the root of our application.
-/// Currently, we just show one widget in our app.
+
 class UnitConverterApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Unit Converter',
-      home: Scaffold(
-        backgroundColor: Colors.green[100],
-        body: Center(
-          // TODO: Determine what properties you'll need to pass into the widget
-
-          child: Category(
-            name: _categoryName,
-            iconLocation: _categoryIcon,
-            color: _categoryColor
-          ),
-        ),
-      ),
+      home: CategoryRoute(),
     );
   }
 }
