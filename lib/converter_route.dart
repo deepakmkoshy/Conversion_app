beb85ea0145ccb362e7ccb401a676d61cb42d72b
Deepak <deepakmkoshy@gmail.com>
2020-07-11 16:36:40 +0530

    Made the category_route and converter_route stateful widgets. End of Lesson 1 in Udacity course

--- lib/converter_route.dart
+++ lib/converter_route.dart
@@ -9,23 +9,30 @@ import 'unit.dart';
 /// While it is named ConverterRoute, a more apt name would be ConverterScreen,
 /// because it is responsible for the UI at the route's destination.
 
-class ConverterRoute extends StatelessWidget {
+class ConverterRoute extends StatefulWidget {
   final Color color;
 
   /// Units for this [Category]
   final List<Unit> units;
+
   const ConverterRoute({
     @required this.color,
     @required this.units,
-  })  : assert(color != null),
+  })
+      : assert(color != null),
         assert(units != null);
 
   @override
+  _ConverterRouteState createState() => _ConverterRouteState();
+
+}
+  class _ConverterRouteState extends State<ConverterRoute>{
+  @override
   Widget build(BuildContext context) {
     // Here is just a placeholder for a list of mock units
-    final unitWidgets = units.map((Unit unit) {
+    final unitWidgets = widget.units.map((Unit unit) {
       return Container(
-        color: color,
+        color: widget.color,
         margin: EdgeInsets.all(8.0),
         padding: EdgeInsets.all(16.0),
         child: Column(
