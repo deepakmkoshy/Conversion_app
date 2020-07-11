c7508ecdbb38b2465254a570e71b7ac0757b3754
Deepak <deepakmkoshy@gmail.com>
2020-07-12 02:37:40 +0530

    Creating converter route page, Status: incomplete

--- lib/category.dart
+++ lib/category.dart
@@ -58,6 +58,7 @@ class Category extends StatelessWidget {
             color: color,
             units: units,
           ),
+          resizeToAvoidBottomPadding: false,
         );
       },
     ));
@@ -80,8 +81,8 @@ class Category extends StatelessWidget {
         height: _rowHeight,
         child: InkWell(
           borderRadius: _borderRadius,
-          highlightColor: color,
-          splashColor: color,
+          highlightColor: color['highlight'],
+          splashColor: color['splash'],
           onTap: () => _navigateToConverter(context),
           child: Padding(
             padding: EdgeInsets.all(8.0),
--- lib/converter_route.dart
+++ lib/converter_route.dart
@@ -1,6 +1,9 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:meta/meta.dart';
 import 'unit.dart';
+
+const _padding = EdgeInsets.all(16.0);
 
 /// Converter screen where users can input amounts to convert.
 ///
@@ -18,15 +21,116 @@ class ConverterRoute extends StatefulWidget {
   const ConverterRoute({
     @required this.color,
     @required this.units,
-  })
-      : assert(color != null),
+  })  : assert(color != null),
         assert(units != null);
 
   @override
   _ConverterRouteState createState() => _ConverterRouteState();
-
 }
-  class _ConverterRouteState extends State<ConverterRoute>{
+
+class _ConverterRouteState extends State<ConverterRoute> {
+  Unit _fromValue;
+  Unit _toValue;
+  double _inputValue;
+  String _convertedValue = '';
+  List<DropdownMenuItem> _unitMenuItems;
+  bool _showValidationError = false;
+
+  @override
+  void initState(){
+    super.initState();
+    _createDropdownMenuItems();
+    _setDefaults();
+  }
+
+  /// Creates fresh list of [DropdownMenuItem] widgets, given a list of [Unit]s.
+  void _createDropdownMenuItems(){
+    var newItems=<DropdownMenuItem>[];
+    for(var unit in widget.units){
+      newItems.add(DropdownMenuItem(
+        value: unit.name,
+        child: Container(
+          child: Text(
+            unit.name,
+            softWrap: true,
+          ),
+        )
+      ));
+    }
+    setState(() {
+      _unitMenuItems=newItems;
+    });
+  }
+
+  //Set the default values for the 'from and 'to'
+  void _setDefaults(){
+    setState(() {
+      _fromValue=widget.units[0];
+      _toValue=widget.units[1];
+    });
+  }
+
+  /// Clean up conversion; trim trailing zeros, e.g. 5.500 -> 5.5, 10.0 -> 10
+  String _format(double conversion) {
+    var outputNum = conversion.toStringAsPrecision(7);
+    if (outputNum.contains('.') && outputNum.endsWith('0')) {
+      var i = outputNum.length - 1;
+      while (outputNum[i] == '0') {
+        i -= 1;
+      }
+      outputNum = outputNum.substring(0, i + 1);
+    }
+    if (outputNum.endsWith('.')) {
+      return outputNum.substring(0, outputNum.length - 1);
+    }
+    return outputNum;
+  }
+
+  void _updateConversion(){
+    setState(() {
+      _convertedValue=_format(_inputValue*(_toValue.conversion/_fromValue.conversion));
+    });
+  }
+
+  void _updateInputValue(String input){
+    setState(() {
+      if(input == null|| input.isEmpty){
+        _convertedValue='';
+      }
+      else{
+        // Even though we are using the numerical keyboard, we still have to check
+        // for non-numerical input such as '5..0' or '6 -3'
+      try {
+        final inputDouble = double.parse(input);
+        _showValidationError = false;
+        _inputValue = inputDouble;
+        _updateConversion();
+      } on Exception catch(e){
+        print('Error: $e');
+        _showValidationError=true;
+      }
+      }
+    });
+  }
+
+  Unit _getUnit(String unitName){
+    return widget.units.firstWhere(
+        (Unit unit) {
+          return unit.name == unitName;
+        },
+        orElse: null,
+        
+    );
+  }
+
+
+
+
+
+
+
+
+
   @override
   Widget build(BuildContext context) {
     // Here is just a placeholder for a list of mock units
@@ -41,10 +145,8 @@ class ConverterRoute extends StatefulWidget {
               unit.name,
               style: Theme.of(context).textTheme.headline,
             ),
-            Text(
-              'Conversion: ${unit.conversion}',
-              style: Theme.of(context).textTheme.subhead
-            )
+            Text('Conversion: ${unit.conversion}',
+                style: Theme.of(context).textTheme.subhead)
           ],
         ),
       );
