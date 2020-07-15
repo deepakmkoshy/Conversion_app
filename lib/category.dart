bdf952220c5a4a984bdba095c9396bb4b28121e7
Deepak <deepakmkoshy@gmail.com>
2020-07-15 21:25:01 +0530

    Updated category_route with backdrop

--- lib/category.dart
+++ lib/category.dart
@@ -6,109 +6,28 @@
 // https://www.dartlang.org/guides/language/effective-dart/style#ordering
 import 'package:flutter/material.dart';
 import 'package:meta/meta.dart';
-import 'package:conversion_app/converter_route.dart';
-import 'package:conversion_app/unit.dart';
 
-final _rowHeight = 100.0;
-final _borderRadius = BorderRadius.circular(_rowHeight / 2);
+import 'unit.dart';
 
-/// A custom [Category] widget.
-///
-/// The widget is composed on an [Icon] and [Text]. Tapping on the widget shows
-/// a colored [InkWell] animation.
-class Category extends StatelessWidget {
+/// A [Category] keeps track of a list of [Unit]s.
+class Category {
   final String name;
   final ColorSwatch color;
-  final IconData iconLocation;
   final List<Unit> units;
+  final IconData iconLocation;
 
-  /// Creates a [Category].
+  /// Information about a [Category].
   ///
-  /// A [Category] saves the name of the Category (e.g. 'Length'), its color for
-  /// the UI, and the icon that represents it (e.g. a ruler).
-  // TODO: You'll need the name, color, and iconLocation from main.dart
+  /// A [Category] saves the name of the Category (e.g. 'Length'), a list of its
+  /// its color for the UI, units for conversions (e.g. 'Millimeter', 'Meter'),
+  /// and the icon that represents it (e.g. a ruler).
   const Category({
-    Key key,
     @required this.name,
     @required this.color,
-    @required this.iconLocation,
     @required this.units,
+    @required this.iconLocation,
   })  : assert(name != null),
         assert(color != null),
-        assert(iconLocation != null),
         assert(units != null),
-        super(key: key);
-
-  /// Navigates to the [ConverterRoute].
-
-  void _navigateToConverter(BuildContext context) {
-    Navigator.of(context).push(MaterialPageRoute<Null>(
-      builder: (BuildContext context) {
-        return Scaffold(
-          appBar: AppBar(
-            elevation: 1.0,
-            title: Text(
-              name,
-              style: Theme.of(context).textTheme.display1,
-            ),
-            centerTitle: true,
-            backgroundColor: color,
-          ),
-          body: ConverterRoute(
-            color: color,
-            units: units,
-          ),
-          resizeToAvoidBottomPadding: false,
-        );
-      },
-    ));
-  }
-
-  /// Builds a custom widget that shows [Category] information.
-  ///
-  /// This information includes the icon, name, and color for the [Category].
-  @override
-  // This `context` parameter describes the location of this widget in the
-  // widget tree. It can be used for obtaining Theme data from the nearest
-  // Theme ancestor in the tree. Below, we obtain the display1 text theme.
-  // See https://docs.flutter.io/flutter/material/Theme-class.html
-  Widget build(BuildContext context) {
-    // TODO: Build the custom widget here, referring to the Specs.
-
-    return Material(
-      color: Colors.transparent,
-      child: Container(
-        height: _rowHeight,
-        child: InkWell(
-          borderRadius: _borderRadius,
-          highlightColor: color['highlight'],
-          splashColor: color['splash'],
-          onTap: () => _navigateToConverter(context),
-          child: Padding(
-            padding: EdgeInsets.all(8.0),
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              //Doubt in above line regarding what the difference is
-              //with mainAxisAlignment, to be cleared in a later stage
-              children: [
-                Padding(
-                    padding: EdgeInsets.all(16.0),
-                    child: Icon(
-                      iconLocation,
-                      size: 60.0,
-                    )),
-                Center(
-                  child: Text(
-                    name,
-                    textAlign: TextAlign.center,
-                    style: Theme.of(context).textTheme.headline,
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
-    );
-  }
+        assert(iconLocation != null);
 }
---
+++ lib/category_tile.dart
@@ -0,0 +1,109 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// To keep your imports tidy, follow the ordering guidelines at
+// https://www.dartlang.org/guides/language/effective-dart/style#ordering
+import 'package:flutter/material.dart';
+import 'package:meta/meta.dart';
+import 'package:conversion_app/unit_converter.dart';
+import 'package:conversion_app/unit.dart';
+import 'category.dart';
+
+final _rowHeight = 100.0;
+final _borderRadius = BorderRadius.circular(_rowHeight / 2);
+
+/// A custom [Category] widget.
+///
+/// The widget is composed on an [Icon] and [Text]. Tapping on the widget shows
+/// a colored [InkWell] animation.
+class CategoryTile extends StatelessWidget {
+  final Category category;
+  final ValueChanged<Category> onTap;
+
+  /// Creates a [Category].
+  ///
+  /// A [Category] saves the name of the Category (e.g. 'Length'), its color for
+  /// the UI, and the icon that represents it (e.g. a ruler).
+  // TODO: You'll need the name, color, and iconLocation from main.dart
+  const CategoryTile({
+    Key key,
+    @required this.category,
+    @required this.onTap,
+  })  : assert(category != null),
+        assert(onTap != null),
+        super(key: key);
+
+  /// Navigates to the [ConverterRoute].
+
+  /*void _navigateToConverter(BuildContext context) {
+    Navigator.of(context).push(MaterialPageRoute<Null>(
+      builder: (BuildContext context) {
+        return Scaffold(
+          appBar: AppBar(
+            elevation: 1.0,
+            title: Text(
+              name,
+              style: Theme.of(context).textTheme.display1,
+            ),
+            centerTitle: true,
+            backgroundColor: color,
+          ),
+          body: ConverterRoute(
+            color: color,
+            units: units,
+          ),
+          resizeToAvoidBottomPadding: false,
+        );
+      },
+    ));
+  }
+*/
+  /// Builds a custom widget that shows [Category] information.
+  ///
+  /// This information includes the icon, name, and color for the [Category].
+  @override
+  // This `context` parameter describes the location of this widget in the
+  // widget tree. It can be used for obtaining Theme data from the nearest
+  // Theme ancestor in the tree. Below, we obtain the display1 text theme.
+  // See https://docs.flutter.io/flutter/material/Theme-class.html
+  Widget build(BuildContext context) {
+    // TODO: Build the custom widget here, referring to the Specs.
+
+    return Material(
+      color: Colors.transparent,
+      child: Container(
+        height: _rowHeight,
+        child: InkWell(
+          borderRadius: _borderRadius,
+          highlightColor: category.color['highlight'],
+          splashColor: category.color['splash'],
+          onTap: () => onTap(category),
+          child: Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              //Doubt in above line regarding what the difference is
+              //with mainAxisAlignment, to be cleared in a later stage
+              children: [
+                Padding(
+                    padding: EdgeInsets.all(16.0),
+                    child: Icon(
+                      category.iconLocation,
+                      size: 60.0,
+                    )),
+                Center(
+                  child: Text(
+                    category.name,
+                    textAlign: TextAlign.center,
+                    style: Theme.of(context).textTheme.headline,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
