fe6dea7e29bf1db8ea31259349b4a785de2a7656
Deepak <deepakmkoshy@gmail.com>
2020-07-22 00:45:45 +0530

    Updated category route to retrieve the currency category using API

--- lib/category_route.dart
+++ lib/category_route.dart
@@ -5,6 +5,8 @@
 import "dart:async";
 import "dart:convert";
 
+import 'package:conversion_app/api.dart';
+
 import 'unit.dart';
 import 'package:flutter/material.dart';
 import 'category.dart';
@@ -89,8 +91,11 @@ class _CategoryRouteState extends State<CategoryRoute> {
     super.didChangeDependencies();
     // We have static unit conversions located in our
     // assets/data/regular_units.json
+    // and we want to also obtain up-to-date Currency conversions from the web
+    // We only want to load our data in once
     if (_categories.isEmpty) {
       await _retrieveLocalCategories();
+      await _retrieveApiCategory();
     }
   }
 
@@ -124,6 +129,41 @@ class _CategoryRouteState extends State<CategoryRoute> {
       });
       categoryIndex+=1;
     });
+  }
+  ///Add the Currency Category retrieved from the API, to our _categories
+  /// Retrieves a [Category] and its [Unit]s from an API on the web
+  Future<void> _retrieveApiCategory() async{
+    // Add a placeholder while we fetch the Currency category using the API
+    setState(() {
+      _categories.add(Category(
+        name: apiCategory['name'],
+        units: [],
+        color: _baseColors.last,
+        iconLocation: _icons.last,
+      ));
+    });
+    final api= Api();
+    final jsonUnits= await api.getUnits(apiCategory['route']);
+    // If the API errors out or we have no internet connection, this category
+    // remains in placeholder mode (disabled)
+    if(jsonUnits!=null){
+      final units=<Unit>[];
+      for(var unit in jsonUnits){
+        units.add(Unit.fromJson(unit));
+      }
+        setState(() {
+          _categories.removeLast();
+          _categories.add(Category(
+            name: apiCategory['name'],
+            units: units,
+            color: _baseColors.last,
+            iconLocation: _icons.last,
+          ));
+        });
+
+    }
+
+
   }
 
   /// Function to call when a [Category] is tapped.
